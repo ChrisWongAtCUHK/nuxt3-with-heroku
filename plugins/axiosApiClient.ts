04eedf645cf7ed2793553cecf37d942d5d8afa82
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-07 13:54:27 +0800

    Use Heroku url

--- plugins/axiosApiClient.ts
+++ plugins/axiosApiClient.ts
@@ -3,11 +3,8 @@ import axios from 'axios';
 export default defineNuxtPlugin((nuxtApp) => {
     const runtimeConfig = useRuntimeConfig()
     const axiosApiClient = axios.create()
-    axiosApiClient.defaults.baseURL = "https://mock-api.binaryboxtuts.com"
-    axiosApiClient.interceptors.request.use(function (config) {
-        config.headers['X-Binarybox-Api-Key'] = runtimeConfig.public.apiKey
-        return config;
-    });
+    // axiosApiClient.defaults.baseURL = "http://localhost:5000";
+    axiosApiClient.defaults.baseURL = "https://heroku-spring-boot-2681ceda9868.herokuapp.com";
     return {
         provide: {
             axiosApiClient: axiosApiClient,
