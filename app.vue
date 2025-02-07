677fc0a332f8f026d05cea2877fe629b725bfaa4
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-07 13:50:26 +0800

    TODO: loading does not show

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- TODO: loading does not show  -->
+  <!-- <NuxtLoadingIndicator color="#fed250" :height="5" :throttle="0" /> -->
   <NuxtLayout>
     <Head>
       <title>Nuxt3 with Heroku</title>
