a4b94cee1eb84325a6323c9968f05a75c0a6ee07
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-07 13:36:49 +0800

    Use Heroku url

--- services/quoteService.js
+++ services/quoteService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
   
 const axiosApiClient = axios.create()
-axiosApiClient.defaults.baseURL = "http://localhost:5000";
+//axiosApiClient.defaults.baseURL = "http://localhost:5000";
+axiosApiClient.defaults.baseURL = "https://heroku-spring-boot-2681ceda9868.herokuapp.com";
 
 export const getQuotes = () =>{
   return axiosApiClient.get('/quotes')
